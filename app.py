189e25876199f7cfc3eef84e362fa1d60cc89963
Yangyang Luo <31087080+Yangyangl7@users.noreply.github.com>
2018-10-13 12:29:52 -0500

    Add auth logic

--- app.py
+++ app.py
@@ -1,35 +1,68 @@
 import os
+import string
+import random
 
-from flask import Flask, g, render_template, url_for
+import json
+from six.moves.urllib.request import urlopen
+from six.moves.urllib.parse import urlencode
+from functools import wraps
+
+from flask import Flask, g, render_template, url_for, abort, jsonify, redirect, request, make_response, session
 import psycopg2
+
+import db
+import auth
 
 app = Flask(__name__)
 
-def connect_db():
-    """Connects to the specific database."""
-    return psycopg2.connect(os.environ.get('DB_DSN'))
+def id_generator(size=13, chars=string.ascii_uppercase + string.digits):
+        return ''.join(random.SystemRandom().choice(chars) for _ in range(size))
 
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    if not hasattr(g, 'pg_db'):
-        g.pg_db = connect_db()
-    return g.pg_db
+@app.before_first_request
+def initialize():
+    db.setup()
+    auth.setup()
+    global auth0
+    auth0 = auth.auth0
 
-@app.teardown_appcontext
-def close_db(error):
-    """Closes the database again at the end of the request."""
-    if hasattr(g, 'db'):
-        g.pg_db.close()
+# Protected Page. Only accessible after login
+def requires_auth(f):
+  @wraps(f)
+  def decorated(*args, **kwargs):
+    if 'profile' not in session:
+      # Redirect to Login page here
+      return redirect('/')
+    return f(*args, **kwargs)
+
+  return decorated
+
+# uncomment following 3 lines when 404 page has been created.
+# @app.errorhandler(404)
+# def page_not_found(error):
+#     return render_template("404.html"), 404
 
 @app.route('/')
 def home():
     return render_template("home.html")
 
+# Auth0 Login
+@app.route('/login')
+def login():
+    return auth0.authorize_redirect(redirect_uri='http://localhost:5000/callback', audience=os.environ['AUTH0_DOMAIN']+'/userinfo')
+
+# Auth0 Logout
+@app.route('/logout')
+def logout():
+    # Clear session stored data
+    session.clear()
+    # Redirect user to logout endpoint
+    params = {'returnTo': url_for('home', _external=True), 'client_id': os.environ['CLIENT_ID']}
+    app.logger.info(auth0.api_base_url + '/v2/logout?' + urlencode(params))
+    return redirect(auth0.api_base_url + '/v2/logout?' + urlencode(params))
 
 
 @app.route('/user/<int:user_id>')
+@requires_auth
 def show_post(user_id):
     # show the post with the given id, the id is an integer
     # sql
@@ -37,9 +70,7 @@ def show_post(user_id):
     # name email avator id, post lists
     return render_template("profile.html")
 
-
 # upload imaage into data base
-
 def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ['png', 'jpg']
