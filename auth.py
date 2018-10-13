792d2b29ee11199f1fc2404efee964be8dfa742f
Yangyang Luo <31087080+Yangyangl7@users.noreply.github.com>
2018-10-13 12:29:09 -0500

    Add auth0.py file

---
+++ auth.py
@@ -0,0 +1,23 @@
+import os
+from flask import current_app, g
+from authlib.flask.client import OAuth
+
+import app
+
+def setup():
+    # Initializing OAuth
+    oauth = OAuth(current_app)
+    current_app.secret_key = app.id_generator()
+
+    global auth0
+    auth0 = oauth.register(
+    'auth0',
+    client_id=os.environ['CLIENT_ID'],
+    client_secret=os.environ['CLIENT_SECRET'],
+    api_base_url=os.environ['AUTH0_DOMAIN'],
+    access_token_url=os.environ['AUTH0_DOMAIN']+'/oauth/token',
+    authorize_url=os.environ['AUTH0_DOMAIN']+'/authorize',
+    client_kwargs={
+        'scope': 'openid profile',
+    },
+)
