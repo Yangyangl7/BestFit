418f407ad30f790100176914c3d64904afc57d69
= <lixx4793@umn.edu>
2018-10-13 11:36:04 -0500

    post form font end

---
+++ static/js/yuhao.js
@@ -0,0 +1,19 @@
+function showPost() {
+$('#postCreater').css('top', '10%');
+$('#scrim').css('display', 'inline-block');
+}
+
+function scrimTriger(caseNum) {
+  switch (caseNum) {
+    case 1:
+      hidePost();
+      break;
+    default:
+      hidePost();
+  }
+}
+
+function hidePost() {
+  $('#postCreater').css('top', '-100%');
+  $('#scrim').css('display', 'none');
+}
