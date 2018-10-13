c8a8af2c9392bd8535a82be2cc9010e94beed1de
Yangyang Luo <31087080+Yangyangl7@users.noreply.github.com>
2018-10-13 12:49:41 -0500

    Delete duplicated route

--- app.py
+++ app.py
@@ -60,23 +60,6 @@ def logout():
     app.logger.info(auth0.api_base_url + '/v2/logout?' + urlencode(params))
     return redirect(auth0.api_base_url + '/v2/logout?' + urlencode(params))
 
-# Auth0 Login
-@app.route('/login')
-def login():
-    return auth0.authorize_redirect(redirect_uri='http://localhost:5000/callback', audience=os.environ['AUTH0_DOMAIN']+'/userinfo')
-
-# Auth0 Logout
-@app.route('/logout')
-def logout():
-    # Clear session stored data
-    session.clear()
-    # Redirect user to logout endpoint
-    params = {'returnTo': url_for('home', _external=True), 'client_id': os.environ['CLIENT_ID']}
-    app.logger.info(auth0.api_base_url + '/v2/logout?' + urlencode(params))
-    return redirect(auth0.api_base_url + '/v2/logout?' + urlencode(params))
-
-
-
 @app.route('/user/<int:user_id>')
 @requires_auth
 def show_post(user_id):
