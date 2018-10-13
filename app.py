eba4f1ccf02ba208cd250047bc883ad4b03ea4fe
Yangyang Luo <31087080+Yangyangl7@users.noreply.github.com>
2018-10-13 17:52:27 -0500

    test

--- app.py
+++ app.py
@@ -122,10 +122,4 @@ def upload():
     return redirect(url_for("home"))
 
 if __name__ == '__main__':
-    app.secret_key = 'super secret key'
-    app.config['SESSION_TYPE'] = 'filesystem'
-
-    sess.init_app(app)
-
-    app.debug = True
     app.run()
