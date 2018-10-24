178b13aa886cc51dc3f3bdfba7474cd5c7fa1bf9
William Batu <williambatu@x-10-145-80-177.wireless.umn.edu>
2018-10-23 22:53:42 -0500

    kkk

--- app.py
+++ app.py
@@ -361,10 +361,11 @@ def post_info(post_id):
         post_comment_time=[record["time"] for record in cur]
         post_comment_reviewer_id=[record["reviewer_id"] for record in cur]
         post_comment_rate=[record["rate"] for record in cur]
-        cur.execute("SELECT * FROM register where id=%s;",
-                    (post_comment_reviewer_id,))
-        post_comment_reviewer_name=[record["name"] for record in cur]
-        post_comment_reviewer_avator=[record["avator"] for record in cur]
+        if (post_comment_reviewer_id!=None):
+            cur.execute("SELECT * FROM register where id=%s;",
+                        (post_comment_reviewer_id,))
+            post_comment_reviewer_name=[record["name"] for record in cur]
+            post_comment_reviewer_avator=[record["avator"] for record in cur]
         cur.execute("SELECT * FROM register where id=%s;",
                     (user_id_res,))
         post_user_name=[record["name"] for record in cur]
